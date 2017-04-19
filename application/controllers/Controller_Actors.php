f93ed8842b87a872d0e1c23f5d2cd696a857fa88
sasha44 <12345@i.ua>
2017-04-19 16:51:17 +0300

    new files

---
+++ application/controllers/Controller_Actors.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Controller_Actors
+ *
+ * @author web
+ */
+class Controller_Actors {
+    //put your code here
+}
