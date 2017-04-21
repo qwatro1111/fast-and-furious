b24228abdab23ea6cad6b02d15097f27e8fddbf0
introqt <introtheintro@gmail.com>
2017-04-21 16:18:33 +0300

    PHP docs Model_Actors.php and Model_Films.php

--- application/models/Model_Actors.php
+++ application/models/Model_Actors.php
@@ -23,20 +23,38 @@ class Model_Actors extends Model {
 /**
  * 
  * @param type $a
- * get files actors from Db 
+ * function get array actors from Db 
  * @return type
  */
     public function get_date($a = NULL) {
+        /**
+         * select all from Db - actors where id = null
+         */
         $query = "SELECT * FROM `actors` WHERE id = ".$a.";";
+        /**
+         * Record the result of the sample in $result
+         */
         $result = $this->db->query($query);
+        /**
+         * $main is empty
+         */
         $main = '';
+        /**
+         * chek if empty go to home page
+         */
         if (empty($result)) {
             $main.= header('Location: /actors?id=1');
         } else {
+            /**
+             *Extract the resulting series in the form of an associative array and generate a suitable variable for the storage representation
+             */
             while($res = $result->fetch_assoc()){
                   $main = serialize($res);
             }
         }
+        /**
+         * return serialize array
+         */
         return $main;
     }
 
