6e0b60d44936ee8aae3ef54a6e4701486b22be4e
introqt <introtheintro@gmail.com>
2017-04-21 15:43:39 +0300

    Made a commit Model_Actors.php and Model_Films.php

--- application/models/Model_Actors.php
+++ application/models/Model_Actors.php
@@ -14,11 +14,18 @@
 class Model_Actors extends Model {
 
     protected $db;
-
+/**
+ * conect to Db
+ */
     public function __construct() {
         $this->db = new mysqli(DB['host'], DB['user'], DB['passwd'], DB['db']);
     }
-
+/**
+ * 
+ * @param type $a
+ * get files actors from Db 
+ * @return type
+ */
     public function get_date($a = NULL) {
         $query = "SELECT * FROM `actors` WHERE id = ".$a.";";
         $result = $this->db->query($query);
